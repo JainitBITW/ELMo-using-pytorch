93c59c50812718340f481ec907bc36676e8bf491
JainitBITW <jainit.bafna@research.iiit.ac.in>
2023-04-27 01:48:48 +0530

    made the file to create language modeling

--- language_model.py
+++ language_model.py
@@ -32,27 +32,23 @@ class Language_model(nn.Module):
     
 
 
-
-
-def train (model , pretrain_loaders, LEARNING_RATE, DEVICE):
+def train (MODEL , pretrain_loaders, LEARNING_RATE, DEVICE , EPOCHS):
     '''pretrain loaders is a dictionary containing the train and validation and test loaders for the pretraining task
-    this function trains the model on the pretraining task
+    this function trains the MODEL on the pretraining task
     '''
-    model.to(DEVICE)
+    MODEL.to(DEVICE)
     criterion = nn.NLLLoss()
 
     # define the optimizer 
-    optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
+    optimizer = torch.optim.Adam(MODEL.parameters(), lr=LEARNING_RATE)
     best_loss = 1000000
     best_accuracy = 0
     def accuracy(output, label):
         output = output.argmax(dim=1)
         return (output == label).float().mean()
     steps = 0
-
     running_loss = 0
-
-    for epoch in range(1):
+    for epoch in range(EPOCHS):
         print('epoch: ', epoch)
         if epoch%3 == 0 and epoch != 0:
             for param_group in optimizer.param_groups:
@@ -60,33 +56,38 @@ def train (model , pretrain_loaders, LEARNING_RATE, DEVICE):
         for input, label in pretrain_loaders['train']:
             steps += 1
             optimizer.zero_grad()
-            model.zero_grad()
+            MODEL.zero_grad()
             input = input.to(DEVICE)
             label = label.to(DEVICE)
-            output = model.forward(input)
+            output = MODEL.forward(input)
             loss = criterion(output, label)
             running_loss += loss.item()
             loss.backward()
             optimizer.step()
             if steps%15 == 0:
-                model.eval()
+                MODEL.eval()
                 with torch.no_grad():
                     val_loss = 0
                     val_accuracy = 0
                     for input, label in pretrain_loaders['validation']:
                         input = input.to(DEVICE)
                         label = label.to(DEVICE)
-                        output = model.forward(input)
+                        output = MODEL.forward(input)
                         val_loss += criterion(output, label)
                         val_accuracy += accuracy(output, label)
                     val_loss = val_loss/len(pretrain_loaders['validation'])
                     val_accuracy = val_accuracy/len(pretrain_loaders['validation'])
                     if val_loss < best_loss:
                         best_loss = val_loss
-                        torch.save(model.state_dict(), 'best_loss.pth')
+                        torch.save(MODEL.state_dict(), 'best_loss.pth')
                     if val_accuracy > best_accuracy:
                         best_accuracy = val_accuracy
-                        torch.save(model.state_dict(), 'best_accuracy.pth')
+                        torch.save(MODEL.state_dict(), 'best_accuracy.pth')
                     print( 'train loss: ', running_loss/100, 'validation loss: ', val_loss, 'validation accuracy: ', val_accuracy)
                     running_loss = 0
-                model.train()
+                MODEL.train()
+
+def make_language_model(Elmo_model, vocab_size, embedding_dim):
+    '''this function makes the language model and trains it on the pretraining task'''
+    MODEL = Language_model(Elmo_model, vocab_size, embedding_dim)
+    return MODEL
